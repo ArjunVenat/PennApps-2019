ba714c045a0d17d497c2732ee7a48ff312bc8862
ArjunVenat <34625035+ArjunVenat@users.noreply.github.com>
2019-09-08 07:57:15 -0400

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -3,8 +3,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import cv2
 
-pause()
-
 driver = webdriver.Chrome("C:/Users/HP/Documents/chromedriver_win32/chromedriver.exe")
 driver.maximize_window()
 driver.get("https://cloud.google.com/automl/ui/vision/datasets/predict?dataset=ICN181326302667377721&model=ICN9201703406336611204&project=pennapps-2019")
